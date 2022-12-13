628c851daaa0c9dcb2f515d7ff733e743c05eb5c
Andre F <50321935+DSFdesigns@users.noreply.github.com>
2022-12-13 09:53:55 +0900

    2022/12/13
    First Commit

---
+++ css/scripts/index.js
@@ -0,0 +1,13 @@
+*{
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+    font-family: 'Poppins', sans-serif;
+}
+
+body{
+    background-color: #333;
+    margin: 0;
+    padding: 0;
+    font-size: 20px;
+}
